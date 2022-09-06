baf7ceaa4280ca42713b41d4ce3f7783066075ff
AgungDH <agungdh@live.com>
2022-09-06 08:56:57 +0000

    migration

---
+++ resources/views/components/menu.blade.php
@@ -0,0 +1,7 @@
+<li class="nav-item">
+    <a class="nav-link" href="/">
+      <svg class="nav-icon">
+        <use xlink:href="/assets/coreui-4.2.1/vendors/@coreui/icons/svg/free.svg#cil-speedometer"></use>
+      </svg> Dashboard
+    </a>
+</li>
